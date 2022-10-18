14f065487325d21ddd9f9099791fd5ccee008cf2
ahmad_bayu_nurdiansyah <ahmad_bayu_nurdiansyah@quick.com>
2022-10-18 08:44:01 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+            "body").style.visibility = "hidden";
+        document.querySelector(
+            "#loading").style.visibility = "visible";
+    } else {
+        document.querySelector(
+            "#loading").style.display = "none";
+        document.querySelector(
+            "body").style.visibility = "visible";
+    }
+};
+
+const links = document.querySelectorAll("nav ul a");
+
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop - 100;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
